cac3a456e30522a4ce3f42b514e4cec4528c8435
markohanesian <mso872@gmail.com>
2020-12-17 16:12:31 -0800

    Updated GitCommand component to display tooltips on hover and tap

--- src/components/GitCommand.js
+++ src/components/GitCommand.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import React from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Button, Tooltip } from "@material-ui/core";
 
 class GitCommand extends React.Component {
-    state = {
-      value: this.props.command,
-      copied: false,
-    };
-   
-    render() {
-      return (
-        <div>
-          <CopyToClipboard text={this.state.value}
-            onCopy={() => this.setState({copied: true})}>
-            <code>{this.props.name}</code>
-          </CopyToClipboard>
+  state = {
+    value: this.props.command,
+    copied: false,
+  };
 
-          {this.state.copied ? <span id="copied-text" >Copied.</span> : null}
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <Tooltip title={this.props.tooltip} placement="right-start">
+          <Button>
+            <CopyToClipboard
+              text={this.state.value}
+              onCopy={() => this.setState({ copied: true })}
+            >
+              <code>{this.props.name}</code>
+            </CopyToClipboard>
+          </Button>
+        </Tooltip>
+
+        {this.state.copied ? <span id="copied-text">Copied.</span> : null}
+      </div>
+    );
   }
-   
-export default GitCommand;
+}
 
+export default GitCommand;
