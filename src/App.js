36d354a1ec6728d99f1d2b1ca67bbb3c5f44e455
markohanesian <mso872@gmail.com>
2020-12-18 08:38:26 -0800

    Updated tooltips be less lengthy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import GitCommand from './components/GitCommand';
+import FadingText from './components/FadingText';
 
 function App() {
   return (
@@ -8,7 +9,6 @@ function App() {
         <code id="title">💻 Git Good 💪🏼</code>
         <p class="subtitle">Click/tap to copy git commands to your clipboard</p>
       </header>
-
       {/* LIST OF GIT COMMANDS */}
       <h2>General Use and Navigation</h2>
       <GitCommand name="pwd" command="pwd" tooltip="display current directory path"/>
@@ -19,19 +19,19 @@ function App() {
       <GitCommand name="mkdir" command="mkdir" tooltip="create directory"/>
       <GitCommand name="touch" command="touch" tooltip="create file"/>
       <h2>Initialize Repo and Version Control</h2>
-      <GitCommand name="git init" command="git init" tooltip="creates an empty Git repository - basically a .git directory with subdirectories for objects , refs/heads , refs/tags , and template files"/>
-      <GitCommand name="git add ." command="git add ." tooltip="adds changes (including new files) in the working directory"/>
-      <GitCommand name="git commit -m" command="git commit -m" tooltip="save your changes to the local repository; sets the commit's message."/>
-      <GitCommand name="git push" command="git push" tooltip="upload local repository content to a remote repository"/>
+      <GitCommand name="git init" command="git init" tooltip="create an empty Git repository"/>
+      <GitCommand name="git add ." command="git add ." tooltip="adds working directory changes"/>
+      <GitCommand name="git commit -m" command="git commit -m" tooltip="save local repository changes; set commit message"/>
+      <GitCommand name="git push" command="git push" tooltip="upload local content to remote repository"/>
       <GitCommand name="git pull" command="git pull" tooltip="download content from a remote repository"/>
-      <GitCommand name="git clone <url to repository>" command="git clone" tooltip="Clones a repository into a newly created directory,"/>
+      <GitCommand name="git clone <url to repository>" command="git clone" tooltip="clone a repository into a new directory"/>
       <GitCommand name="git checkout -b <branch name>" command="git checkout -b" tooltip="switch working changes to a new or existing branch"/>
 
       <h2>Delete and Others</h2>
       <GitCommand name="git rm --cached <file Name>" command="git rm --cached" tooltip="remove file from staging area"/>
       <GitCommand name="git rm <file Name>" command="git rm" tooltip="remove file from staging area and file system"/>
       <h2>Help!</h2>
-      <GitCommand name="git status" command="git status" tooltip="displays the state of the working directory and the staging area, including whether you need to pull remote changes to our local repository"/>
+      <GitCommand name="git status" command="git status" tooltip="displays working directory state"/>
       <GitCommand name="git reset HEAD~" command="git reset HEAD~" tooltip="undo previous commit"/>
       <GitCommand name="git reflog expire --expire-unreachable=now --all" command="git reflog expire --expire-unreachable=now --all" tooltip="removes all references of unreachable commits in reflog"/>
       <GitCommand name="git gc --prune=now" command="git gc --prune=now" tooltip="removes loose commits regardless of age"/>
