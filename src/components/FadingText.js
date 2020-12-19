72c6fb8e6e437ce9710931b47f06638383bee944
markohanesian <mso872@gmail.com>
2020-12-19 15:28:26 -0800

    Updated "Copied" text styling

--- src/components/FadingText.js
+++ src/components/FadingText.js
@@ -1,7 +1,7 @@
 import {useSpring, animated} from 'react-spring'
 
 function FadingText() {
-  const props = useSpring({config: { duration: 1500 }, opacity: 0, from: {opacity: 1}})
+  const props = useSpring({config: { duration: 2000 }, opacity: 0, from: {opacity: 1, padding: '5px', backgroundColor: 'lightgreen', color: 'white'}})
   return <animated.div style={props}>copied</animated.div>
 }
 
